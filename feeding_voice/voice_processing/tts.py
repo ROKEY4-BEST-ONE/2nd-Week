8bef34755f476b8462db071cd9f22c910539a2e6
SGKwon95 <ssunder7@naver.com>
2025-08-04 10:35:55 +0900

    tts class 구현

--- feeding_voice/voice_processing/tts.py
+++ feeding_voice/voice_processing/tts.py
@@ -3,31 +3,45 @@ from playsound import playsound
 from ament_index_python.packages import get_package_share_directory
 import os
 
+package_path = get_package_share_directory("feeding_voice")
+tts_file_list = ['not_recognized.mp3']
+# tts_file_list = ['not_recognized.mp3', 'menu_introducing.mp3']
+
+NOT_RECOGNIZED = 0
+# MENU_INTRODUCING = 1
+
 class TTS:
-    def __init__(self, text):
+    def __init__(self, condition, target = ''):
+        self.condition = condition
+        if condition == NOT_RECOGNIZED:
+            if target == 'rice':
+                subtext = '밥을'
+            elif target == 'pororo' or target == 'loopy':
+                subtext = '숟가락을'
+            elif target == 'pikachu' or target == 'Bulbasaur':
+                subtext = '포크를'
+            elif target == 'Broccoli':
+                subtext = '브로콜리를'
+            elif target == 'Croissant':
+                subtext = '크로와상을'
+            elif target == 'apple':
+                subtext = '사과를'
+            text = f"카메라가 {subtext} 인식하지 못했습니다."
         self.tts = gTTS(text=text, lang='ko', slow=False)
-        if 
-        self.tts.save(file_path)
-        self.
+
+        for filename in tts_file_list:
+            filepath = os.path.join(package_path, "resource", filename)
+            # filepath = os.path.join(os.getcwd(), filename)
+            print(filepath)
+            self.tts.save(filepath)
     
     def play(self):
-        playsound(file_path)
-        p
-# 텍스트 정의
-text = ""
+        try:
+            playsound(os.path.join(package_path, "resource", tts_file_list[self.condition]))
+            # playsound(os.path.join(os.getcwd(), tts_file_list[self.condition]))
+        except Exception as e:
+            print(e)
 
-# gTTS 객체 생성
-# lang='ko'는 한국어를 의미하며, 다양한 언어 코드를 사용할 수 있습니다.
-tts = gTTS(text=text, lang='ko', slow=False)
-
-# 음성 파일로 저장
-file_path = "hello.mp3"
-tts.save(file_path)
-
-# 음성 파일 재생
-print("음성 파일 재생 중...")
-playsound(file_path)
-
-# 파일 삭제 (선택 사항)
-os.remove(file_path)
-print("음성 파일 삭제 완료.")
+# if __name__ == '__main__':
+#     tts = TTS(NOT_RECOGNIZED, 'apple')
+#     tts.play()
